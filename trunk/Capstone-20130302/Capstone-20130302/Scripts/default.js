bef40a6bdbf609374910e76c39eb67190192b508
phong.lekhoi@gmail.com <phong.lekhoi@gmail.com@adfbc1ca-80e6-6e3d-81bd-b1316cd96128>
2013-03-28 11:57:47 +0000

    Fix cart

--- trunk/Capstone-20130302/Capstone-20130302/Scripts/default.js
+++ trunk/Capstone-20130302/Capstone-20130302/Scripts/default.js
@@ -1,19 +1,3 @@
 $(function(){
-    $(".modal").on("show",function(){
-        $(".modal").not(this).modal("hide");
-    });
-    //$('[rel=tooltip]').tooltip();
-    $("#cart > a").click(function(){
-        $("#cart > .cart-container").toggle(300);
-    })
-    $(document).click(function(){
-        $(".cart-container").hide(200);
-    })
-    $(".cart-container,#cart > a").click(function (e) {
-        e.preventDefault();
-        return false;
-    });
-    $("[class*=logo], #logo").click(function () {
-        location.href = "/";
-    });
+    
 })
