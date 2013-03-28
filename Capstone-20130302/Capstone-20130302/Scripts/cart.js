c040e59af4807f651edd5dbf4e700eefb8991bb2
phong.lekhoi@gmail.com <phong.lekhoi@gmail.com@adfbc1ca-80e6-6e3d-81bd-b1316cd96128>
2013-03-28 11:57:47 +0000

    Fix cart

---
+++ Capstone-20130302/Capstone-20130302/Scripts/cart.js
@@ -0,0 +1,103 @@
+﻿function CartViewModel() {
+    var self = this;
+    self.shops = ko.observableArray([]);
+    self.addProduct = function (product) {
+        if (product instanceof ProductModel) {
+            if (product.shopId != -1) {
+                self.shops().forEach(function (shop) {
+                    if (shop.shopId == product.shopId) {
+                        var checkIfExistProduct = 0;
+                        var pro
+                        shop.products().forEach(function (p) {
+                            if (product.productId == p.productId) {
+                                checkIfExistProduct++;
+                                p.productQuantity(p.productQuantity() + product.productQuantity());
+                            }
+                        })
+                        if (checkIfExistProduct == 0) {
+                            shop.products.push(product);
+                        }
+                    }
+                    self.saveToStorage();
+                    return;
+                })
+            }
+        }
+    }
+    self.checkShop = function (shop) {
+        var i = 0;
+        self.shops().forEach(function (sh) {
+            if (sh.shopId == shop.shopId) {
+                i++;
+                return
+            }
+        })
+        if (i == 0) {
+            self.shops.push(shop);
+        }
+    }
+    self.saveToStorage = function () {
+        var dummyCart = []
+        self.shops().forEach(function (s) {
+            var shop = [];
+            shop.push(s.shopId);
+            shop.push(s.storeTitle);
+
+            s.products().forEach(function (p) {
+                var product = [];
+                product.push(p.productId);
+                product.push(p.productTitle);
+                product.push(p.productImage);
+                product.push(p.productPrice);
+                product.push(p.productQuantity());
+
+                shop.push(product)
+            })
+            dummyCart.push(shop);
+        })
+        var json = JSON.stringify(dummyCart);
+        localStorage.setItem("sb_cartData", json);
+    }
+    self.loadFromStorage = function () {
+        var json = localStorage.getItem("sb_cartData");
+        if (json == null) return;
+        var dataObject = JSON.parse(json);
+        dataObject.forEach(function (s) {
+            var shop = new ShopModel(s[1], s[0])
+            shop.products.push(new ProductModel(s[2][1], s[2][2], s[2][3], s[2][4], s[2][0], s[0]));
+            //s[2].forEach(function(p){
+            //    shop.products.push(new ProductModel(p[1], p[2], p[3],p[4],p[0],s[0]));
+            //})
+            cartmodel.shops.push(shop)
+        })
+    }
+}
+function ShopModel(title, shopId) {
+    var self = this;
+    self.shopId = typeof shopId == "undefined" ? -1 : shopId;
+    self.storeTitle = title;
+    self.products = ko.observableArray([])
+    self.removeProduct = function (product, $root) {
+        self.products.remove(product);
+        if (self.products().length == 0) {
+            $root.shops.remove(self);
+        }
+        $root.saveToStorage();
+    }
+}
+function ProductModel(title, image, price, quantity, productId, shopId) {
+    var self = this;
+    self.productId = typeof productId == "undefined" ? -1 : productId;
+    self.shopId = typeof shopId == "undefined" ? -1 : shopId;
+
+    self.productTitle = title;
+    self.productImage = image;
+    self.productPrice = price;
+    self.productQuantity = ko.observable(quantity);
+}
+
+var cartmodel = new CartViewModel();
+cartmodel.loadFromStorage();
+$(function () {
+    ko.applyBindings(cartmodel);
+})
